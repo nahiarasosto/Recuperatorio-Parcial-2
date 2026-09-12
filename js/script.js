5814c4c10f242922b606d6d7ff84108f4d252813
NahiaraSosto <nahiarasosto@gmail.com>
2026-09-11 23:11:21 -0300

    Terminando la galería, comenzando con el ejercicio
    También eliminando archivo de texto que ya no es necesario

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+/*ACLARACIÓN: tuve un problema que al ejecutar un evento 
+la consola mostraba error porque intentaba leer al mismo tiempo eventos pertenecientes a otras páginas del sitio
+Usar if fue la mejor solución que encontré
+*/
+
 // DATOS CURIOSOS
 
 let datosCuriosos = [
@@ -13,34 +18,59 @@ let datosCuriosos = [
     'Además de visuales digitales, ha realizado impresiones generativas de gran formato como obras únicas o en series.'
 ];
 
-let botonDatosCuriosos = document.querySelector('button');
+let botonDatosCuriosos = document.querySelector('#botonCurioso');
 let pantallaDatosCuriosos = document.querySelector('.bloqueCurioso');
 
-botonDatosCuriosos.addEventListener('click', function () {
 
-    let indiceAleatorio = Math.floor(Math.random() * datosCuriosos.length);
+if (botonDatosCuriosos && pantallaDatosCuriosos) {
+    botonDatosCuriosos.addEventListener('click', function () {
 
-    let resultado = datosCuriosos[indiceAleatorio];
+        let indiceAleatorio = Math.floor(Math.random() * datosCuriosos.length);
+        
+        let resultado = datosCuriosos[indiceAleatorio];
+        
+        pantallaDatosCuriosos.innerHTML = resultado;
+    
+    });
+}
 
-    pantallaDatosCuriosos.innerHTML = resultado;
-
-});
+// INFO.HTML
 
 // GALERÍA DE OBRAS
 
 let galeria = document.querySelector('.galeriaDeObras');
-
-galeria.innerHTML =
-    '<div class="obra"><img src="../img/reas-1.jpg" alt="Imagen de la serie Still Life, 2016">' +
-    '<p>Still life, 2016</p></div>' +
-    '<div class="obra"><img src="../img/reas-4.jpg" alt="Imagen de la obra CSRSNT-MRIE-24-OF-32.PNG, 2025">' +
-    '<p>CSRSNT-MRIE-24-OF-32.PNG, 2025</p></div>' +
-    '<div class="obra"><img src="../img/reas-2.jpg" alt="Imagen de la serie Path, 2001">' +
-    '<p>Path, 2001</p></div>' +
-    '<div class="obra"><img src="../img/reas-5.jpg" alt="Imagen de la obra CENTURY-XXX-METAJUDD, 2025">' +
-    '<p>CENTURY-XXX-METAJUDD, 2025</p></div>' +
-    '<div class="obra"><img src="../img/reas-6.jpg" alt="Imagen de la obra Study for a Garden of Earthly Delights, No. 4, 2018">' +
-    '<p>Study for a Garden of Earthly Delights, No. 4, 2018</p></div>';
-
-    
 let botonGaleria = document.querySelector('#botonGaleria');
+
+if (galeria && botonGaleria) {
+
+    // GALERÍA INICIAL
+
+    galeria.innerHTML =
+        '<div class="obra"><img src="../img/reas-1.jpg" alt="Imagen de la serie Still Life, 2016">' +
+        '<p>Still life, 2016</p></div>' +
+        '<div class="obra"><img src="../img/reas-4.jpg" alt="Imagen de la obra CSRSNT-MRIE-24-OF-32.PNG, 2025">' +
+        '<p>CSRSNT-MRIE-24-OF-32.PNG, 2025</p></div>' +
+        '<div class="obra"><img src="../img/reas-2.jpg" alt="Imagen de la serie Path, 2001">' +
+        '<p>Path, 2001</p></div>' +
+        '<div class="obra"><img src="../img/reas-5.jpg" alt="Imagen de la obra CENTURY-XXX-METAJUDD, 2025">' +
+        '<p>CENTURY-XXX-METAJUDD, 2025</p></div>' +
+        '<div class="obra"><img src="../img/reas-6.jpg" alt="Imagen de la obra Study for a Garden of Earthly Delights, No. 4, 2018">' +
+        '<p>Study for a Garden of Earthly Delights, No. 4, 2018</p></div>';
+
+    // CAMBIA LAS OBRAS DE LA GALERÍA AL HACER CLICK
+
+    botonGaleria.addEventListener('click', function () {
+        galeria.innerHTML =
+            '<div class="obra"><img src="../img/reas-3.jpg" alt="Imagen de la serie Still Life, 2016">' +
+            '<p>Still life, 2016</p></div>' +
+            '<div class="obra"><img src="../img/reas-7.jpg" alt="Imagen de la obra DETAIL OF PROCESS 13 from PRocess compendium, 2010">' +
+            '<p>DETAIL OF PROCESS 13 from PRocess compendium, 2010</p></div>' +
+            '<div class="obra"><img src="../img/reas-8.jpg" alt="Imagen de la obra Still from atoms, 2023">' +
+            '<p>Still from atoms, 2023</p></div>' +
+            '<div class="obra"><img src="../img/reas-9.jpg" alt="Imagen de la obra Microimage A-06, 2002">' +
+            '<p>Microimage A-06, 2002</p></div>' +
+            '<div class="obra"><img src="../img/reas-10.jpg" alt="Imagen de la serie Details of process, 2005">' +
+            '<p>Details of process, 2005</p></div>';
+    });
+}
+
